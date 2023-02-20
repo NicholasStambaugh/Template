7789316ca677a38b3fe04359b80017f1342266bf
Nick Stambaugh <73977662+NicholasStambaugh@users.noreply.github.com>
2023-02-20 17:34:07 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MySurvey from './components/surveytype/surveytypeone';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <MySurvey />
+    </div>
+  );
+}
+
+export default App;
